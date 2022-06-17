f1dd18021b8e46eb3b7076df7f4424d41b1821a6
Pukkysasikan <sasikan.in.62@ubu.ac.th>
2022-06-17 21:46:15 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'vuedemo',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
